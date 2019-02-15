79caac7be135c2644fa1a8e7b3c2304badb17529
Guilherme Lanza <guilherme.lanza@hotmail.com>
2019-02-15 20:46:12 -0200

    Aula 15/02 pt2

--- src/main/java/br/com/lanza/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/lanza/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package br.com.lanza.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.lanza.cursomc.domain.Categoria;
 import br.com.lanza.cursomc.repositories.CategoriaRepository;
+import br.com.lanza.cursomc.services.exceptions.DataIntegrityException;
 import br.com.lanza.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,10 +17,30 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 
 }
