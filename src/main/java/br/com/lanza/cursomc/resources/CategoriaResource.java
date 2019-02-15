8064e751ac1392b302444dbef1ddbcb8aac59bb1
Guilherme Lanza <guilherme.lanza@hotmail.com>
2019-02-15 21:15:53 -0200

    Aula 15/02 pt3

--- src/main/java/br/com/lanza/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/lanza/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.lanza.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.lanza.cursomc.domain.Categoria;
+import br.com.lanza.cursomc.dto.CategoriaDTO;
 import br.com.lanza.cursomc.services.CategoriaService;
 
 @RestController
@@ -40,11 +43,19 @@ public class CategoriaResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
