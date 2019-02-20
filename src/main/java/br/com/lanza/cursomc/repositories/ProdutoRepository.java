e43beb63d2d1a3a7908401980010cd1d1069cabf
guilhermelanza <guilherme.lanza@hotmail.com>
2019-02-20 10:38:15 -0300

    Aula 20/fev

--- src/main/java/br/com/lanza/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/br/com/lanza/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,25 @@
 package br.com.lanza.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.lanza.cursomc.domain.Categoria;
 import br.com.lanza.cursomc.domain.Produto;
 
 @Repository
+@Transactional(readOnly = true)
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
