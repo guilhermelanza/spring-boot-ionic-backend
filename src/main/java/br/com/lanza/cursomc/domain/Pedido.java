364df76d761d5d7a970642a7889432424eea14b1
guilhermelanza <guilherme.lanza@hotmail.com>
2019-02-21 11:20:33 -0300

    Aula 21/fev

--- src/main/java/br/com/lanza/cursomc/domain/Pedido.java
+++ src/main/java/br/com/lanza/cursomc/domain/Pedido.java
@@ -1,11 +1,9 @@
 package br.com.lanza.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
