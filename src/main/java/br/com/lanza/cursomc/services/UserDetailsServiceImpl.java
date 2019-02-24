1ec551721df6538baa2080bd9915c4957e137646
Guilherme Lanza <lanzagdev@gmail.com>
2019-02-24 11:45:07 -0300

    Aula 24/fev pt2 jwt token add

---
+++ src/main/java/br/com/lanza/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.lanza.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.lanza.cursomc.domain.Cliente;
+import br.com.lanza.cursomc.repositories.ClienteRepository;
+import br.com.lanza.cursomc.security.UserSS;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository clienteRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cliente = clienteRepo.findByEmail(email);
+		if (cliente == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cliente.getId(), cliente.getEmail(), cliente.getSenha(), cliente.getPerfis());
+	}
+
+}
